4e62a7de029f5218d2a1656ea2598fcbb224eaf1
Ludovit-ArchVM <ludovit.34@gmail.com>
2022-09-20 19:52:29 +0800

    log in, log out

---
+++ pb_public/scripts/username.mjs
@@ -0,0 +1,10 @@
+import PocketBase from '/scripts/pocketbase.es.mjs'
+var $ = function (id) { return document.getElementById(id) }
+const client = new PocketBase('http://localhost:8090');
+
+console.log(client.authStore.isValid);
+if (client.authStore.isValid) { //if is logged in
+    const userModel = client.authStore.model
+    //alert(userModel.email)
+    $('login').innerText = `${userModel.email}`
+}
