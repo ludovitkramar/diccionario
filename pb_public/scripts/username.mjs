faee2c0ecd06b382dbc339e1d69badbdceb9f454
Ludovit-ArchVM <ludovit.34@gmail.com>
2022-09-20 23:45:06 +0800

    añadir palabras y definiciones

--- pb_public/scripts/username.mjs
+++ pb_public/scripts/username.mjs
@@ -2,7 +2,7 @@ import PocketBase from '/scripts/pocketbase.es.mjs'
 var $ = function (id) { return document.getElementById(id) }
 const client = new PocketBase('http://localhost:8090');
 
-console.log(client.authStore.isValid);
+console.log('client.authStore.isValid', client.authStore.isValid);
 if (client.authStore.isValid) { //if is logged in
     const userModel = client.authStore.model
     //alert(userModel.email)
